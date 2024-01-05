0cb16d303ff68f9cc2410c1fbec77256d00f93d0
mi-wada <mtak.wd@gmail.com>
2024-01-05 10:03:13 +0900

    Impl mkdir -p

--- src/10/mkdir/main.rs
+++ src/10/mkdir/main.rs
@@ -2,14 +2,31 @@ use anyhow::{anyhow, Result};
 
 fn main() -> Result<()> {
     let dir_name = std::env::args().nth(1).unwrap();
+    let recursive = match std::env::args().nth(2) {
+        Some(arg) => arg == "-p",
+        None => false,
+    };
 
-    mkdir(&dir_name)
+    mkdir(&dir_name, recursive)
 }
 
-fn mkdir(dir_name: &str) -> Result<()> {
+fn mkdir(dir_name: &str, recursive: bool) -> Result<()> {
     unsafe {
-        if libc::mkdir(dir_name.as_ptr() as _, libc::S_IRWXU) == -1 {
+        if !recursive && libc::mkdir(dir_name.as_ptr() as _, libc::S_IRWXU) == -1 {
             return Err(anyhow!("Failed to create dir"));
+        }
+
+        if recursive {
+            let mut path = std::path::PathBuf::new();
+            for component in dir_name.split('/') {
+                path.push(component);
+                if libc::mkdir(path.to_str().unwrap().as_ptr() as _, libc::S_IRWXU) == -1 {
+                    match std::io::Error::last_os_error().raw_os_error().unwrap() {
+                        libc::EEXIST => {}
+                        _ => return Err(anyhow!("Failed to create dir")),
+                    }
+                }
+            }
         }
     }
     Ok(())
--- tests/mkdir_test.rs
+++ tests/mkdir_test.rs
@@ -14,3 +14,19 @@ fn test_success() {
         cmd.arg(&target_dir_name).assert();
     }
 }
+
+#[test]
+fn test_success_p() {
+    let target_dir_name = format!("tmp/not_exist_dir_{}/mkdir_test", rand::random::<u32>());
+
+    let mut cmd = Command::cargo_bin("mkdir").unwrap();
+    let assert = cmd.arg(&target_dir_name).arg("-p").assert();
+    assert.success();
+
+    {
+        let path = std::path::Path::new(&target_dir_name);
+        let mut cmd = Command::cargo_bin("rmdir").unwrap();
+        cmd.arg(path.to_str().unwrap()).assert();
+        cmd.arg(path.parent().unwrap().to_str().unwrap()).assert();
+    }
+}
